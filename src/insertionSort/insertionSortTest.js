492d059276941fe484ca944ae4197a0f93507078
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2021-02-09 22:45:53 +0100

    update insertion sort algorithm

--- src/insertionSort/insertionSortTest.js
+++ src/insertionSort/insertionSortTest.js
@@ -1,3 +1,6 @@
-console.log(insertionSort([1, 2, 1, 5]));
+// test on normal input
+console.log(insertionSort([1, 2, 1, 5, 10, 1, 15, 2]));
+// test on empty input
 console.log(insertionSort([]));
+// test on input with single value
 console.log(insertionSort([5]));
